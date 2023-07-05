a7739e9c59f5d57c810c438a180027bd6ae6197a
DjurovicAleksandar <djur.aleksandar@gmail.com>
2023-07-05 15:01:57 +0200

    Adding bar

--- src/components/pages/skills/Coaching.jsx
+++ src/components/pages/skills/Coaching.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Coaching() {
-  return <div>Coaching</div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center font-medium text-2xl">
+      U IZRADI
+    </div>
+  );
 }
 
 export default Coaching;
--- src/components/sidebars/Navigation.jsx
+++ src/components/sidebars/Navigation.jsx
@@ -7,8 +7,10 @@ import linkedin from "../../assets/images/socials/linkedin.png";
 import youtube from "../../assets/images/socials/youtube.png";
 
 import logo from "../../assets/images/logo.png";
+import { useState } from "react";
 
 function NavigationBar({ animationVariants }) {
+  const [showMenu, setShowMenu] = useState(false);
   const location = useLocation();
 
   const pageNav = [
@@ -62,79 +64,135 @@ function NavigationBar({ animationVariants }) {
 
   return (
     <motion.div
-      className={`w-full px-4 py-8 fixed top-0 z-[2000] font-medium flex items-center justify-between sm:w-[95%] sm:mx-auto`}
+      className={`fixed top-0 z-[2000] font-medium w-full  sm:mx-auto`}
       initial={animationVariants.initial}
       animate={animationVariants.animate}
       transition={animationVariants.transition}
     >
-      <div className="w-[5rem] sm:w-[7rem] lg:w-[10rem]">
-        <img className="w-full  translate-x-[-30%]" alt="Logo" src={logo} />
-      </div>
-      <button className="text-xs sm:hidden">Meni</button>
-      <nav className="hidden sm:flex  translate-x-[-30%] ">
-        {pageNav.map((e, i) => {
-          return (
-            <div
-              key={i}
-              className={`${
-                i !== pageNav.length - 1 && "mr-4"
-              } relative group text-xs lg:text-[14px] group`}
-            >
-              <Link to={e.href}>{e.title}</Link>
-              {e.submenu ? (
-                <ul
-                  className={`absolute top-5 left-0 right-0 z-[100] hidden ${
-                    e.submenu && "group-hover:block"
+      <div className="w-full relative  lg:px-4 py-8 flex items-center justify-between">
+        <div className="w-[5rem] sm:w-[7rem] lg:w-[10rem] px-2">
+          <img className="w-full  translate-x-[-30%]" alt="Logo" src={logo} />
+        </div>
+        <button
+          onClick={() => {
+            setShowMenu(true);
+          }}
+          className="text-xs sm:hidden text-oliveDark"
+        >
+          Meni
+        </button>
+        <nav className={`hidden sm:flex ranslate-x-[-30%]`}>
+          {pageNav.map((e, i) => {
+            return (
+              <div
+                key={i}
+                className={`${
+                  i !== pageNav.length - 1 && "mr-4"
+                } relative group text-xs lg:text-[14px] group`}
+              >
+                <Link to={e.href}>{e.title}</Link>
+                {e.submenu ? (
+                  <ul
+                    className={`absolute top-5 left-0 right-0 z-[100] hidden ${
+                      e.submenu && "group-hover:block"
+                    }`}
+                  >
+                    {e.submenu.map((dropdown, i) => {
+                      return (
+                        <motion.li whileHover={{ scale: 1.1 }} key={i}>
+                          <Link to={dropdown.href}>{dropdown.title}</Link>
+                        </motion.li>
+                      );
+                    })}
+                  </ul>
+                ) : null}
+
+                <span
+                  className={`absolute inline-block left-0 -bottom-0.5 h-[0.5px] group-hover:w-full duration-300 ease-in-out bg-dark ${
+                    location.pathname === e.href ? "w-full" : "w-0"
                   }`}
                 >
-                  {e.submenu.map((dropdown, i) => {
-                    return (
-                      <motion.li whileHover={{ scale: 1.1 }} key={i}>
-                        <Link to={dropdown.href}>{dropdown.title}</Link>
-                      </motion.li>
-                    );
-                  })}
-                </ul>
-              ) : null}
+                  &nbsp;
+                </span>
+              </div>
+            );
+          })}
+        </nav>
 
-              <span
-                className={`absolute inline-block left-0 -bottom-0.5 h-[0.5px] group-hover:w-full duration-300 ease-in-out bg-dark ${
-                  location.pathname === e.href ? "w-full" : "w-0"
-                }`}
-              >
-                &nbsp;
-              </span>
+        {showMenu && (
+          <nav
+            className={`fixed w-full h-[20vh] bg-oliveDark/90 sm:hidden text-white  flex flex-col items-center justify-center gap-10 z-[2000]`}
+          >
+            <div className="flex">
+              {pageNav.map((e, i) => {
+                return (
+                  <div
+                    key={i}
+                    className={`${
+                      i !== pageNav.length - 1 && "mr-4"
+                    } relative group text-xs lg:text-[14px] group`}
+                  >
+                    <Link to={e.href}>{e.title}</Link>
+                    {e.submenu ? (
+                      <ul
+                        className={`absolute top-5 left-0 right-0 z-[100] hidden ${
+                          e.submenu && "group-hover:block"
+                        }`}
+                      >
+                        {e.submenu.map((dropdown, i) => {
+                          return (
+                            <motion.li whileHover={{ scale: 1.1 }} key={i}>
+                              <Link to={dropdown.href}>{dropdown.title}</Link>
+                            </motion.li>
+                          );
+                        })}
+                      </ul>
+                    ) : null}
+
+                    <span
+                      className={`absolute inline-block left-0 -bottom-0.5 h-[0.5px] group-hover:w-full duration-300 ease-in-out bg-dark ${
+                        location.pathname === e.href ? "w-full" : "w-0"
+                      }`}
+                    >
+                      &nbsp;
+                    </span>
+                  </div>
+                );
+              })}
             </div>
-          );
-        })}
-      </nav>
 
-      {/* <div className="absolute left-[50%] translate-x-[-50%]">
-        <img className="w-[10rem]" alt="Logo" src={logo} />
-      </div> */}
-
-      <nav
-        className="flex gap-1 
-      "
-      >
-        {socials.map((social) => {
-          return (
-            <motion.a
-              key={social.src + 1}
-              href={social.href}
-              target={"_blank"}
-              whileHover={{ y: -5 }}
-              whileTap={{ scale: 0.9 }}
+            <button
+              className="rounded-full border-white border-[1px] px-3 py-1 hover:bg-white hover:text-oliveDark hover:border-oliveDark cursor-pointer ease-linear duration-300"
+              onClick={() => setShowMenu(false)}
             >
-              <img
-                src={social.src}
-                alt={social.src.slice(social.src.lastIndexOf("/") + 1)}
-                className="w-[1rem] sm:w-[1.2rem] lg:w-[1.4rem]"
-              />
-            </motion.a>
-          );
-        })}
-      </nav>
+              X
+            </button>
+          </nav>
+        )}
+
+        <nav
+          className="flex gap-1 px-2 lg:px-0
+      "
+        >
+          {socials.map((social) => {
+            return (
+              <motion.a
+                key={social.src + 1}
+                href={social.href}
+                target={"_blank"}
+                whileHover={{ y: -5 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <img
+                  src={social.src}
+                  alt={social.src.slice(social.src.lastIndexOf("/") + 1)}
+                  className="w-[1rem] sm:w-[1.2rem] lg:w-[1.4rem]"
+                />
+              </motion.a>
+            );
+          })}
+        </nav>
+      </div>
     </motion.div>
   );
 }
